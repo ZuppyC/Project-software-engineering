074f081836fd7d0cbef50947ea2ef2d95e6fdad2
Matii <raganmateusz28@gmail.com>
2026-03-11 23:42:54 +0100

    TESTEN op HAPPYDAY scenario waarbij alles bij xml file juist is

--- SoftwareEng/Tests/Tests.cpp
+++ SoftwareEng/Tests/Tests.cpp
@@ -29,6 +29,69 @@ protected:
 
 
 
+class HAPPYDAY_SCENARIO: public ::testing::Test {
+protected:
+    friend class System;
+    // You should make the members protected s.t. they can be
+    // accessed from sub-classes.
+
+    // virtual void SetUp() will be called before each test is run.  You
+    // should define it if you need to initialize the variables.
+    // Otherwise, this can be skipped.
+    virtual void SetUp() {
+    }
+
+    // virtual void TearDown() will be called after each test is run.
+    // You should define it if there is cleanup work to do.  Otherwise,
+    // you don't have to provide it.
+    virtual void TearDown() {
+    }
+
+    // Declares the variables your tests want to use.
+    System happydaysss_;
+
+};
+
+TEST_F(HAPPYDAY_SCENARIO,xmlIsJuist)
+{
+    TiXmlDocument doc("../Main_Directory/xmlfile.xml");
+    ASSERT_TRUE(doc.LoadFile());
+    TiXmlElement* root = doc.RootElement();
+    EXPECT_STREQ("SYSTEM",root->Value());
+
+    for (TiXmlElement* childs = root->FirstChildElement(); childs!= NULL;childs = childs->NextSiblingElement())
+    {
+        string type = childs->Value();
+        if (type == "ROOM")
+        {
+            EXPECT_NE(nullptr,childs->FirstChildElement("NAME"));
+            EXPECT_NE(nullptr,childs->FirstChildElement("CAPACITY"));
+            EXPECT_NE(nullptr,childs->FirstChildElement("IDENTIFIER"));
+
+            EXPECT_GT(stoi(childs->FirstChildElement("CAPACITY")->GetText()),0);
+        }
+        else if (type == "MEETING")
+        {
+            EXPECT_NE(nullptr,childs->FirstChildElement("LABEL"));
+            EXPECT_NE(nullptr,childs->FirstChildElement("ROOM"));
+            EXPECT_NE(nullptr,childs->FirstChildElement("IDENTIFIER"));
+            EXPECT_NE(nullptr,childs->FirstChildElement("DATE"));
+        }
+        else if (type == "PARTICIPATION") {
+            EXPECT_NE(nullptr,childs->FirstChildElement("MEETING"));
+            EXPECT_NE(nullptr,childs->FirstChildElement("USER"));
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
 TEST_F(SYSTEMTESTS, NoRoomViolation)
 {
     EXPECT_DEATH(sss_.parser("../xmlfilesTests/NoRoom.xml"), "De XML bestand moet ROOM element hebben.");
@@ -44,6 +107,9 @@ TEST_F(SYSTEMTESTS, NoMeetingViolation)
     EXPECT_DEATH(sss_.parser("../xmlfilesTests/NoMeeting.xml"), "De XML bestand moet MEETINGS element hebben.");
 
 }
+
+
+
 
 
 
