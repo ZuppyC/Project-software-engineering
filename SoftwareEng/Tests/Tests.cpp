475903cf6f1f691bed094f0fcc950b32b9ba0e40
Zakariya Mastour <zakariya.mastour@student.uantwerpen.be>
2026-05-01 15:51:57 +0200

    happyday test geschreven voor SystemOutput:  SystemOutputIsJuist

--- SoftwareEng/Tests/Tests.cpp
+++ SoftwareEng/Tests/Tests.cpp
@@ -3,6 +3,7 @@
 #include "iostream"
 #include "../xmlparser/tinyxml.h"
 #include "../Main_Directory/Output.h"
+#include "compare_file.h"
 
 class SYSTEMTESTS: public ::testing::Test {
 protected:
@@ -52,40 +53,6 @@ protected:
 };
 
 
-
-//  BRON SERGE DEMEYER  functie FileCompare.
-// Copyright   : Project Software Engineering - BA1 Informatica - Serge Demeyer - University of Antwerp
-bool FileCompare(const std::string leftFileName, const std::string rightFileName) {
-    ifstream leftFile, rightFile;
-    char leftRead, rightRead;
-    bool result;
-
-    // Open the two files.
-    leftFile.open(leftFileName.c_str());
-    if (!leftFile.is_open()) {
-        return false;
-    };
-    rightFile.open(rightFileName.c_str());
-    if (!rightFile.is_open()) {
-        leftFile.close();
-        return false;
-    };
-
-    result = true; // files exist and are open; assume equality unless a counterexamples shows up.
-    while (result && leftFile.good() && rightFile.good()) {
-        leftFile.get(leftRead);
-        rightFile.get(rightRead);
-        result = (leftRead == rightRead);
-    };
-    if (result) {
-        // last read was still equal; are we at the end of both files ?
-        result = (!leftFile.good()) && (!rightFile.good());
-    };
-
-    leftFile.close();
-    rightFile.close();
-    return result;
-}
 
 
 TEST_F(SYSTEMTESTS, SyntaxFoutXML_VAL)
