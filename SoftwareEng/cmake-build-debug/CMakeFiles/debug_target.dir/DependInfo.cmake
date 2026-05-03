875784716021326f1a96e84f7a1c96ad3dce6596
Amanallah Horchani <amengaamer1@gmail.com>
2026-05-03 20:02:07 +0200

    Voeg tests en statistiekenrapport toe

---
+++ SoftwareEng/cmake-build-debug/CMakeFiles/debug_target.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/amens/Downloads/pp1/SoftwareEng/Main_Directory/Building.cpp" "CMakeFiles/debug_target.dir/Main_Directory/Building.cpp.obj" "gcc" "CMakeFiles/debug_target.dir/Main_Directory/Building.cpp.obj.d"
+  "C:/Users/amens/Downloads/pp1/SoftwareEng/Main_Directory/Campus.cpp" "CMakeFiles/debug_target.dir/Main_Directory/Campus.cpp.obj" "gcc" "CMakeFiles/debug_target.dir/Main_Directory/Campus.cpp.obj.d"
+  "C:/Users/amens/Downloads/pp1/SoftwareEng/Main_Directory/Catering.cpp" "CMakeFiles/debug_target.dir/Main_Directory/Catering.cpp.obj" "gcc" "CMakeFiles/debug_target.dir/Main_Directory/Catering.cpp.obj.d"
+  "C:/Users/amens/Downloads/pp1/SoftwareEng/Main_Directory/Input.cpp" "CMakeFiles/debug_target.dir/Main_Directory/Input.cpp.obj" "gcc" "CMakeFiles/debug_target.dir/Main_Directory/Input.cpp.obj.d"
+  "C:/Users/amens/Downloads/pp1/SoftwareEng/Main_Directory/Meeting.cpp" "CMakeFiles/debug_target.dir/Main_Directory/Meeting.cpp.obj" "gcc" "CMakeFiles/debug_target.dir/Main_Directory/Meeting.cpp.obj.d"
+  "C:/Users/amens/Downloads/pp1/SoftwareEng/Main_Directory/Output.cpp" "CMakeFiles/debug_target.dir/Main_Directory/Output.cpp.obj" "gcc" "CMakeFiles/debug_target.dir/Main_Directory/Output.cpp.obj.d"
+  "C:/Users/amens/Downloads/pp1/SoftwareEng/Main_Directory/Participation.cpp" "CMakeFiles/debug_target.dir/Main_Directory/Participation.cpp.obj" "gcc" "CMakeFiles/debug_target.dir/Main_Directory/Participation.cpp.obj.d"
+  "C:/Users/amens/Downloads/pp1/SoftwareEng/Main_Directory/Renovation.cpp" "CMakeFiles/debug_target.dir/Main_Directory/Renovation.cpp.obj" "gcc" "CMakeFiles/debug_target.dir/Main_Directory/Renovation.cpp.obj.d"
+  "C:/Users/amens/Downloads/pp1/SoftwareEng/Main_Directory/Room.cpp" "CMakeFiles/debug_target.dir/Main_Directory/Room.cpp.obj" "gcc" "CMakeFiles/debug_target.dir/Main_Directory/Room.cpp.obj.d"
+  "C:/Users/amens/Downloads/pp1/SoftwareEng/Main_Directory/System.cpp" "CMakeFiles/debug_target.dir/Main_Directory/System.cpp.obj" "gcc" "CMakeFiles/debug_target.dir/Main_Directory/System.cpp.obj.d"
+  "C:/Users/amens/Downloads/pp1/SoftwareEng/Tests/InputTests.cpp" "CMakeFiles/debug_target.dir/Tests/InputTests.cpp.obj" "gcc" "CMakeFiles/debug_target.dir/Tests/InputTests.cpp.obj.d"
+  "C:/Users/amens/Downloads/pp1/SoftwareEng/Tests/Tests.cpp" "CMakeFiles/debug_target.dir/Tests/Tests.cpp.obj" "gcc" "CMakeFiles/debug_target.dir/Tests/Tests.cpp.obj.d"
+  "C:/Users/amens/Downloads/pp1/SoftwareEng/Tests/compare_file.cpp" "CMakeFiles/debug_target.dir/Tests/compare_file.cpp.obj" "gcc" "CMakeFiles/debug_target.dir/Tests/compare_file.cpp.obj.d"
+  "C:/Users/amens/Downloads/pp1/SoftwareEng/xmlparser/tinystr.cpp" "CMakeFiles/debug_target.dir/xmlparser/tinystr.cpp.obj" "gcc" "CMakeFiles/debug_target.dir/xmlparser/tinystr.cpp.obj.d"
+  "C:/Users/amens/Downloads/pp1/SoftwareEng/xmlparser/tinyxml.cpp" "CMakeFiles/debug_target.dir/xmlparser/tinyxml.cpp.obj" "gcc" "CMakeFiles/debug_target.dir/xmlparser/tinyxml.cpp.obj.d"
+  "C:/Users/amens/Downloads/pp1/SoftwareEng/xmlparser/tinyxmlerror.cpp" "CMakeFiles/debug_target.dir/xmlparser/tinyxmlerror.cpp.obj" "gcc" "CMakeFiles/debug_target.dir/xmlparser/tinyxmlerror.cpp.obj.d"
+  "C:/Users/amens/Downloads/pp1/SoftwareEng/xmlparser/tinyxmlparser.cpp" "CMakeFiles/debug_target.dir/xmlparser/tinyxmlparser.cpp.obj" "gcc" "CMakeFiles/debug_target.dir/xmlparser/tinyxmlparser.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ SoftwareEng/cmake-build-debug/CMakeFiles/release_target.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/amens/Downloads/pp1/SoftwareEng/Main_Directory/Building.cpp" "CMakeFiles/release_target.dir/Main_Directory/Building.cpp.obj" "gcc" "CMakeFiles/release_target.dir/Main_Directory/Building.cpp.obj.d"
+  "C:/Users/amens/Downloads/pp1/SoftwareEng/Main_Directory/Campus.cpp" "CMakeFiles/release_target.dir/Main_Directory/Campus.cpp.obj" "gcc" "CMakeFiles/release_target.dir/Main_Directory/Campus.cpp.obj.d"
+  "C:/Users/amens/Downloads/pp1/SoftwareEng/Main_Directory/Catering.cpp" "CMakeFiles/release_target.dir/Main_Directory/Catering.cpp.obj" "gcc" "CMakeFiles/release_target.dir/Main_Directory/Catering.cpp.obj.d"
+  "C:/Users/amens/Downloads/pp1/SoftwareEng/Main_Directory/Input.cpp" "CMakeFiles/release_target.dir/Main_Directory/Input.cpp.obj" "gcc" "CMakeFiles/release_target.dir/Main_Directory/Input.cpp.obj.d"
+  "C:/Users/amens/Downloads/pp1/SoftwareEng/Main_Directory/Meeting.cpp" "CMakeFiles/release_target.dir/Main_Directory/Meeting.cpp.obj" "gcc" "CMakeFiles/release_target.dir/Main_Directory/Meeting.cpp.obj.d"
+  "C:/Users/amens/Downloads/pp1/SoftwareEng/Main_Directory/Output.cpp" "CMakeFiles/release_target.dir/Main_Directory/Output.cpp.obj" "gcc" "CMakeFiles/release_target.dir/Main_Directory/Output.cpp.obj.d"
+  "C:/Users/amens/Downloads/pp1/SoftwareEng/Main_Directory/Participation.cpp" "CMakeFiles/release_target.dir/Main_Directory/Participation.cpp.obj" "gcc" "CMakeFiles/release_target.dir/Main_Directory/Participation.cpp.obj.d"
+  "C:/Users/amens/Downloads/pp1/SoftwareEng/Main_Directory/Renovation.cpp" "CMakeFiles/release_target.dir/Main_Directory/Renovation.cpp.obj" "gcc" "CMakeFiles/release_target.dir/Main_Directory/Renovation.cpp.obj.d"
+  "C:/Users/amens/Downloads/pp1/SoftwareEng/Main_Directory/Room.cpp" "CMakeFiles/release_target.dir/Main_Directory/Room.cpp.obj" "gcc" "CMakeFiles/release_target.dir/Main_Directory/Room.cpp.obj.d"
+  "C:/Users/amens/Downloads/pp1/SoftwareEng/Main_Directory/System.cpp" "CMakeFiles/release_target.dir/Main_Directory/System.cpp.obj" "gcc" "CMakeFiles/release_target.dir/Main_Directory/System.cpp.obj.d"
+  "C:/Users/amens/Downloads/pp1/SoftwareEng/Main_Directory/main.cpp" "CMakeFiles/release_target.dir/Main_Directory/main.cpp.obj" "gcc" "CMakeFiles/release_target.dir/Main_Directory/main.cpp.obj.d"
+  "C:/Users/amens/Downloads/pp1/SoftwareEng/xmlparser/tinystr.cpp" "CMakeFiles/release_target.dir/xmlparser/tinystr.cpp.obj" "gcc" "CMakeFiles/release_target.dir/xmlparser/tinystr.cpp.obj.d"
+  "C:/Users/amens/Downloads/pp1/SoftwareEng/xmlparser/tinyxml.cpp" "CMakeFiles/release_target.dir/xmlparser/tinyxml.cpp.obj" "gcc" "CMakeFiles/release_target.dir/xmlparser/tinyxml.cpp.obj.d"
+  "C:/Users/amens/Downloads/pp1/SoftwareEng/xmlparser/tinyxmlerror.cpp" "CMakeFiles/release_target.dir/xmlparser/tinyxmlerror.cpp.obj" "gcc" "CMakeFiles/release_target.dir/xmlparser/tinyxmlerror.cpp.obj.d"
+  "C:/Users/amens/Downloads/pp1/SoftwareEng/xmlparser/tinyxmlparser.cpp" "CMakeFiles/release_target.dir/xmlparser/tinyxmlparser.cpp.obj" "gcc" "CMakeFiles/release_target.dir/xmlparser/tinyxmlparser.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
